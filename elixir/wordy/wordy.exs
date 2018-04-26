b0f7cafd5582eb143ba283552fe3755954fbb6ed
Joe Webb <jwebb@weedmaps.com>
2018-04-26 08:22:11 -0700

    Pass tests for multiple operations

--- elixir/wordy/wordy.exs
+++ elixir/wordy/wordy.exs
@@ -37,9 +37,20 @@ defmodule Operation do
       [operand_1, operator, operand_2] ->
         parse_single_binary_operation(operand_1, operator, operand_2)
 
+      [operand_1, operator_1, operand_2, operator_2, operand_3] ->
+        parse_two_binary_operation(operand_1, operator_1, operand_2, operator_2, operand_3)
+
       _ ->
         {:error, :failed_to_parse}
     end
+  end
+
+  def execute(
+        %Operation{
+          operands: [%Operation{} = inner_operation, operand]
+        } = outer_operation
+      ) do
+    execute(%{outer_operation | operands: [execute(inner_operation), operand]})
   end
 
   def execute(%Operation{operator: operator} = operation) do
@@ -54,6 +65,26 @@ defmodule Operation do
        %Operation{
          operator: operator,
          operands: [operand_1, operand_2]
+       }}
+    end
+  end
+
+  defp parse_two_binary_operation(operand_1, operator_1, operand_2, operator_2, operand_3) do
+    with {:ok, operand_1} <- Operand.parse(operand_1),
+         {:ok, operator_1} <- Operator.parse(operator_1),
+         {:ok, operand_2} <- Operand.parse(operand_2),
+         {:ok, operator_2} <- Operator.parse(operator_2),
+         {:ok, operand_3} <- Operand.parse(operand_3) do
+      {:ok,
+       %Operation{
+         operator: operator_2,
+         operands: [
+           %Operation{
+             operator: operator_1,
+             operands: [operand_1, operand_2]
+           },
+           operand_3
+         ]
        }}
     end
   end
