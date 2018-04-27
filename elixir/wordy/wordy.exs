52860421e331c8068441f45bec4b04d023fca2b8
Joe Webb <jwebb@weedmaps.com>
2018-04-26 19:17:46 -0700

    Refactoring

--- elixir/wordy/wordy.exs
+++ elixir/wordy/wordy.exs
@@ -4,92 +4,21 @@ defmodule Wordy do
   """
   @spec answer(String.t()) :: integer
   def answer(question) do
-    with {:ok, operation} <- Question.to_operation(question) do
-      Operation.execute(operation)
-    end
-  end
-end
-
-defmodule Question do
-  def to_operation(question) do
-    question
-    |> trim
-    |> Operation.parse()
-  end
-
-  defp trim(question) do
     question
     |> String.trim_leading("What is ")
     |> String.trim_trailing("?")
-    |> String.replace("by ", "")
-  end
-end
-
-defmodule Operation do
-  @fields ~w(operator operands)a
-  @enforce_keys @fields
-  defstruct(@fields)
-
-  def parse(operation) do
-    operation
     |> String.split()
-    |> parse_operations()
+    |> Enum.flat_map(&parse_token/1)
+    |> solve()
   end
 
-  def execute(
-        %Operation{
-          operands: [%Operation{} = inner_operation, operand]
-        } = outer_operation
-      ) do
-    execute(%{outer_operation | operands: [execute(inner_operation), operand]})
-  end
+  defp parse_token("by"), do: []
+  defp parse_token("plus"), do: [&Kernel.+/2]
+  defp parse_token("minus"), do: [&Kernel.-/2]
+  defp parse_token("multiplied"), do: [&Kernel.*/2]
+  defp parse_token("divided"), do: [&Kernel.div/2]
+  defp parse_token(token), do: [String.to_integer(token)]
 
-  def execute(%Operation{operator: operator} = operation) do
-    apply(Kernel, operator, operation.operands)
-  end
-  
-  defp parse_operations(operations, operation_struct \\ %Operation{operator: nil, operands: []})
-  defp parse_operations([operand, operator |rest] = operations, operation_struct) do
-    with {:ok, parsed_operand} <- Operand.parse(operand),
-         {:ok, parsed_operator} <- Operator.parse(operator)
-    do
-      if operation_struct.operator && length(operation_struct.operands)==1 do
-        new_operation_struct = %{operation_struct | operands: operation_struct.operands ++ [parsed_operand]}
-        new_operand = execute(new_operation_struct)
-        parse_operations(rest, %Operation{operands: [new_operand], operator: parsed_operator})
-      else
-        new_operation_struct = %{operation_struct | operands: operation_struct.operands ++ [parsed_operand], operator: parsed_operator}
-        parse_operations(rest, new_operation_struct)
-      end
-    else
-      _ -> raise ArgumentError
-    end
-  end
-  defp parse_operations([operand], operation_struct) do
-    case Operand.parse(operand) do
-      {:ok, verified} -> 
-        {:ok, %{operation_struct | operands: operation_struct.operands ++ [verified]}}
-      anything -> anything
-    end
-  end 
-end
-
-defmodule Operand do
-  def parse(operand) do
-    case Integer.parse(operand) do
-      {integer, ""} -> {:ok, integer}
-      :error -> {:error, :failed_to_parse_operand, operand}
-    end
-  end
-end
-
-defmodule Operator do
-  def parse("plus"), do: {:ok, :+}
-  def parse("minus"), do: {:ok, :-}
-  def parse("multiplied"), do: {:ok, :*}
-  def parse("divided"), do: {:ok, :div}
-
-  def parse(operator) do
-    {:error, :failed_to_parse_operator, operator}
-  end
+  defp solve([result]), do: result
+  defp solve([int_1, op, int_2 | tokens]), do: solve([op.(int_1, int_2) | tokens])
 end
