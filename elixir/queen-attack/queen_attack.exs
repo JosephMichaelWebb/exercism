7c1d938ca4f5f9affaa630abd1aa45c9ab8fd6b8
Joe Webb <jwebb@weedmaps.com>
2018-05-01 08:57:29 -0700

    Refactoring

--- elixir/queen-attack/queen_attack.exs
+++ elixir/queen-attack/queen_attack.exs
@@ -19,74 +19,38 @@ defmodule Queens do
   white and black queen locations shown
   """
   @spec to_string(Queens.t()) :: String.t()
-  def to_string(queens) do
-    {row, column} = queens.white
-    white_index = row * 16 + column * 2
-    {row, column} = queens.black
-    black_index = row * 16 + column * 2
+  def to_string(%{black: black, white: white}) do
+    white_index = index(white)
+    black_index = index(black)
 
-    to_string(white_index, black_index, "", 0)
-  end
-
-  def to_string(_white_index, _black_index, acc, 127) do
-    acc
-  end
-
-  def to_string(white_index, black_index, acc, index)
-      when rem(index + 1, 16) == 0 do
-    to_string(white_index, black_index, acc <> "\n", index + 1)
-  end
-
-  def to_string(white_index, black_index, acc, index)
-      when rem(index, 2) == 1 do
-    to_string(white_index, black_index, acc <> " ", index + 1)
-  end
-
-  def to_string(white_index, black_index, acc, black_index) do
-    to_string(white_index, black_index, acc <> "B", black_index + 1)
-  end
-
-  def to_string(white_index, black_index, acc, white_index) do
-    to_string(white_index, black_index, acc <> "W", white_index + 1)
-  end
-
-  def to_string(white_index, black_index, acc, index) do
-    to_string(white_index, black_index, acc <> "_", index + 1)
+    0..127
+    |> Enum.reduce("", fn
+      127, acc -> acc
+      i, acc when rem(i + 1, 16) == 0 -> acc <> "\n"
+      i, acc when rem(i, 2) == 1 -> acc <> " "
+      ^black_index, acc -> acc <> "B"
+      ^white_index, acc -> acc <> "W"
+      _i, acc -> acc <> "_"
+    end)
   end
 
   @doc """
   Checks if the queens can attack each other
   """
   @spec can_attack?(Queens.t()) :: boolean
-  def can_attack?(%{black: {row, _}, white: {row, _}}) do
-    true
-  end
-
-  def can_attack?(%{black: {_, col}, white: {_, col}}) do
-    true
-  end
+  def can_attack?(%{black: {row, _}, white: {row, _}}), do: true
+  def can_attack?(%{black: {_, col}, white: {_, col}}), do: true
 
   for n <- 1..7 do
     def can_attack?(%{black: {b_row, b_col}, white: {w_row, w_col}})
-        when w_col == b_col + unquote(n) and w_row == b_row + unquote(n) do
-      true
-    end
-
-    def can_attack?(%{black: {b_row, b_col}, white: {w_row, w_col}})
-        when b_col == w_col + unquote(n) and b_row == w_row + unquote(n) do
-      true
-    end
-
-    def can_attack?(%{black: {b_row, b_col}, white: {w_row, w_col}})
-        when b_col == w_col - unquote(n) and b_row == w_row + unquote(n) do
-      true
-    end
-
-    def can_attack?(%{black: {b_row, b_col}, white: {w_row, w_col}})
-        when b_col == w_col + unquote(n) and b_row == w_row - unquote(n) do
-      true
-    end
+        when (w_col == b_col + unquote(n) and w_row == b_row + unquote(n)) or
+               (b_col == w_col + unquote(n) and b_row == w_row + unquote(n)) or
+               (b_col == w_col - unquote(n) and b_row == w_row + unquote(n)) or
+               (b_col == w_col + unquote(n) and b_row == w_row - unquote(n)),
+        do: true
   end
 
   def can_attack?(_queens), do: false
+
+  defp index({row, column}), do: row * 16 + column * 2
 end
