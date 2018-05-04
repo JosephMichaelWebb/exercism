3ab412a01c3741169cacce172e9b904cfffd22d7
Joe Webb <jwebb@weedmaps.com>
2018-05-04 12:04:35 -0700

    Refactor

--- elixir/queen-attack/queen_attack.exs
+++ elixir/queen-attack/queen_attack.exs
@@ -6,11 +6,15 @@ defmodule Queens do
   Creates a new set of Queens
   """
   @spec new() :: Queens.t()
-  @spec new({integer, integer}, {integer, integer}) :: Queens.t()
-
   def new(), do: %Queens{}
+
+  @spec new({integer, integer}, {integer, integer}) :: Queens.t()
   def new(queen, queen), do: raise(ArgumentError)
   def new(white, black), do: %Queens{white: white, black: black}
+
+  defguard end_of_line(i) when rem(i + 1, 16) == 0
+  defguard between_space(i) when rem(i, 2) == 1
+  @last_index 127
 
   @doc """
   Gives a string reprentation of the board with
@@ -21,28 +25,32 @@ defmodule Queens do
     white_index = index(white)
     black_index = index(black)
 
-    Enum.reduce(0..127, "", fn
-      127, acc -> acc
-      i, acc when rem(i + 1, 16) == 0 -> acc <> "\n"
-      i, acc when rem(i, 2) == 1 -> acc <> " "
+    Enum.reduce(0..@last_index, "", fn
+      @last_index, acc -> acc
+      i, acc when end_of_line(i) -> acc <> "\n"
+      i, acc when between_space(i) -> acc <> " "
       ^black_index, acc -> acc <> "B"
       ^white_index, acc -> acc <> "W"
       _i, acc -> acc <> "_"
     end)
   end
 
-  defp index({row, column}), do: row * 16 + column * 2
+  defguard is_diagonal(w_col, w_row, b_col, b_row) when abs(w_col - b_col) == abs(w_row - b_row)
 
   @doc """
   Checks if the queens can attack each other
   """
   @spec can_attack?(Queens.t()) :: boolean
-  def can_attack?(%{black: {row, _}, white: {row, _}}), do: true
-  def can_attack?(%{black: {_, col}, white: {_, col}}), do: true
-
   def can_attack?(%{black: {b_row, b_col}, white: {w_row, w_col}})
-      when abs(w_col - b_col) == abs(w_row - b_row),
-      do: true
+      when b_row == w_row
+      when b_col == w_col
+      when is_diagonal(w_col, w_row, b_col, b_row) do
+    true
+  end
 
-  def can_attack?(_queens), do: false
+  def can_attack?(_queens) do
+    false
+  end
+
+  defp index({row, column}), do: row * 16 + column * 2
 end
--- elixir_benchmark/scripts/queen_attack.exs
+++ elixir_benchmark/scripts/queen_attack.exs
@@ -4,7 +4,8 @@ queens = Queens.new({4, 3}, {2, 7})
 
 Benchee.run(
   %{
-    "new/2" => fn -> Queens.new(queens) end,
+    "new/0" => fn -> Queens.new() end,
+    "new/2" => fn -> Queens.new({4, 3}, {2, 7}) end,
     "to_string/1" => fn -> Queens.to_string(queens) end,
     "can_attack?/1" => fn -> Queens.can_attack?(queens) end
   },
