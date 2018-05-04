88a58801bfc65b640e33ede689cd38887798d09a
Joe Webb <jwebb@weedmaps.com>
2018-05-04 11:15:36 -0700

    use abs

--- elixir/queen-attack/queen_attack.exs
+++ elixir/queen-attack/queen_attack.exs
@@ -40,14 +40,9 @@ defmodule Queens do
   def can_attack?(%{black: {row, _}, white: {row, _}}), do: true
   def can_attack?(%{black: {_, col}, white: {_, col}}), do: true
 
-  for n <- 1..7 do
-    def can_attack?(%{black: {b_row, b_col}, white: {w_row, w_col}})
-        when w_col == b_col + unquote(n) and w_row == b_row + unquote(n)
-        when b_col == w_col + unquote(n) and b_row == w_row + unquote(n)
-        when b_col == w_col - unquote(n) and b_row == w_row + unquote(n)
-        when b_col == w_col + unquote(n) and b_row == w_row - unquote(n),
-        do: true
-  end
+  def can_attack?(%{black: {b_row, b_col}, white: {w_row, w_col}})
+      when abs(w_col - b_col) == abs(w_row - b_row),
+      do: true
 
   def can_attack?(_queens), do: false
 end
